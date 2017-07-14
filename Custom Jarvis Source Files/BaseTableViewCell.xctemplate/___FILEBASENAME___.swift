f62bf740f5f8c2eaa53ac96ca2c834df41d3ad78
joey <joeynelson42@gmail.com>
2017-07-14 11:49:26 -0600

    added BaseTableViewCell

---
+++ Custom Jarvis Source Files/BaseTableViewCell.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,42 @@
+//
+//  ___FILEBASENAME___.swift
+//
+
+import UIKit
+
+class ___FILEBASENAME___: UITableViewCell {
+    
+    // MARK: - Subviews
+    
+    
+    // MARK: - Stored Constraints
+    // (Store any constraints that might need to be changed or animated later)
+    
+    
+    // MARK: - Initialization
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureSubviews()
+        configureLayout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    /// Set view/subviews appearances
+    fileprivate func configureSubviews() {
+        
+    }
+    
+    /// Add subviews, set layoutMargins, initialize stored constraints, set layout priorities, activate constraints
+    fileprivate func configureLayout() {
+        
+        // Activate NSLayoutAnchors within this closure
+        NSLayoutConstraint.activate([
+            
+            ])
+    }
+}
+
