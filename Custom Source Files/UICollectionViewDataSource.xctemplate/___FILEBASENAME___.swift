66a3d9ad90dad75a1789ba53707b260bc8bf9832
joey <joeynelson42@gmail.com>
2018-01-31 11:43:33 -0700

    added collectionView dataSource

---
+++ Custom Source Files/UICollectionViewDataSource.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,25 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import UIKit
+
+class ___FILEBASENAME___: NSObject, UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "", for: indexPath)
+        return cell
+    }
+}
