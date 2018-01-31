260139928d59218759d8c5b0a9e2c69f21ddfafa
joey <joeynelson42@gmail.com>
2018-01-31 11:47:02 -0700

    added tableView dataSource

---
+++ Custom Source Files/UITableViewDataSource.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,25 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import UIKit
+
+class ___FILEBASENAME___: NSObject, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "")
+        return cell
+    }
+}
